e6c7bec7937cb90ca651ff149fe1dfe89438a85f
jotavasquez <32280840+jotavasquez@users.noreply.github.com>
2017-11-12 19:38:17 -0300

    v 0.04
    Funciona tecla enter en Textarea
    Mejoras y limpieza de código puesto como comentarios.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,7 @@
 //tomar elemento boton
 var btn = document.getElementById("btn");
 
-/*
-//Poner contador en html
-//crear nodo de texto que tenga la cantidad e letras escritas
-var nodeCount = document.createTextNode("280");
-//crear elemento donde colocar el nodo contador de letras
-var count = document.createElement("p");
-//agregar atributo
-count.setAttribute("id", "countNumber");
-//agregar nodo al elemento p
-count.appendChild(nodeCount);
-//llamar al div padre addComment
-var addComment = document.getElementById("addComment");
-// colocar contador en el div comment
-addComment.appendChild(count);
-*/
-
+//BOTON GUARDAR TWEETS
 // tomar evento para elemento boton y ejecutar funcion que valida y agrega los tweets
 btn.addEventListener("click", function () {
 	//toma texto ingresado del textarea
@@ -31,8 +16,7 @@ btn.addEventListener("click", function () {
 		alert('Debes ingresar un mensaje');
 		return false;
 	}
-
-	//Poner comentarios:
+	//PONER COMENTARIOS:
 	//llamo al contenedor padre que esta en el html
 	var cont = document.getElementById('cont');
 	//crea nodos de texto para el textarea desde comment
@@ -47,6 +31,7 @@ btn.addEventListener("click", function () {
 	cont.appendChild(newComments);
 });
 
+
 //FUNCION CUENTA REGRESIVA
 //llamo al textArea
 var textArea = document.getElementById("comment");
@@ -54,7 +39,6 @@ var textArea = document.getElementById("comment");
 document.getElementById("comment").focus();
 //llamo al contenedor del contador
 var contNumber = document.getElementById("count");
-
 
 //funcion que cuenta los caracteres
 function counter() {
@@ -66,7 +50,6 @@ function counter() {
 	contNumber.innerHTML = number;
 	console.log(number);
 
-	//ciclo  if probando
 	if (number < 20 && number > 10) {
 		contNumber.setAttribute("id", "yellow");
 		document.getElementById('btn').disabled = false;
@@ -83,31 +66,7 @@ function counter() {
 	}
 };
 
-
-
-/*
-//toma texto ingresado del textarea
-var textarea = document.getElementById('comment');
-
-textarea.addEventListener("onkeydown", function () {
-	console.log(comment.length);
-	//crear nodo de texto que tenga la cantidad e letras escritas
-	var nodeCount = document.createTextNode(comment.value.length);
-	//crear elemento donde colocar el nodo contador de letras
-	var count = document.createElement("p");
-	//agregar atributo
-	count.setAttribute("id", "countNumber");
-	//agregar nodo al elemento p
-	count.appendChild(nodeCount);
-	//llamar al div padre addComment
-	var addComment = document.getElementById("addComment");
-	// colocar contador en el div comment
-	addComment.appendChild(count);
-
-
-});
-*/
-
+//TEXTAREA AJUSTAR ALTURA AUTOMATICA
 // funcion para hacer crecer el texarea mientras se escribe
 var textarea = document.getElementById("comment"); // tomar textarea
 //oninput ejecuta javascript inmediatamente despues de que el textArea recibe datos del usuario
@@ -126,11 +85,7 @@ textarea.oninput = function () {
 };
 
 
-
-
-
-
-
+/*
 //funcion autoinvocada para la hora del tweet
 (function () {
 	// instantiate a JavaScript Date object
@@ -139,45 +94,4 @@ textarea.oninput = function () {
 	var eDisplayDate = document.getElementById('cont');
 	eDisplayDate.innerHTML = NowDate;
 })();
-
-
-/*
-// Función propia que no me resultó
-function count() {
-	var tweets = document.getElementById('comment').value;
-	var characters = document.getElementById("characters");
-	var charNumber = tweets.length;
-	console.log(charNumber);
-
-	var contChar = document.createElement('p');
-	contChar.appendChild(charNumber);
-	characters.appendChild(contChar);
-}
-
-
-//insertar función autoejecutable código desde internet que no entendí del todo
-(function () {
-	var comment = document.getElementById("comment")[0], // toma el elemento id desde el primer indice
-		charLeftLabel = "characters", // nombre de la clase para la variable contador de caracteres
-		charLeft = document.getElementsByClassName(charLeftLabel)[0],
-		maxChar = 140, // máximo de caracteres
-		maxCharWarn = 20; //minimo de caracteres
-
-	// muestra los caracteres desde el principio
-	charLeft.innerHTML = maxChar; // innerHTML escribe en el html el contador
-
-	// actualiza mientras se tipea
-	comment.onkeydown = function () { // onkeydown cuando el usuario aprieta una tecla en textarea
-		setTimeout(function () { // setTimeout es un contador de javascript, actualiza la funcion después de un segundo de tipear
-			charLeft.innerHTML = maxChar - comment.length; 
-
-			// muestra una advertencia para el contador
-			var warnLabel = comment.length >= maxChar - maxCharWarn ? " warning" : "";
-			charLeft.className = charLeftLabel + warnLabel;
-		}, 1); // segundo estimado para la función setTimeout
-	};
-})();
-
 */
-
-
